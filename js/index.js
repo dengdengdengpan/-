1ee5c2785d35d20b02ca0e6ad3e2ed923eb49024
dengdengdengpan <1306631506@qq.com>
2018-04-07 15:30:07 +0800

    完成index.html页面中#topbar,#content .page-rec的html和css

---
+++ js/index.js
@@ -0,0 +1,15 @@
+let tabsLis = document.querySelectorAll('#content .tabs>li'),
+    tabsCtLis = document.querySelectorAll('#content .tabs-content>li');
+tabsLis.forEach(function(tabsli) {
+    tabsli.addEventListener('click',function() {
+        tabsLis.forEach(function(li) {
+            li.classList.remove('active');
+        });
+        this.classList.add('active');
+        let index = [].indexOf.call(tabsLis,this);
+        tabsCtLis.forEach(function(tabsCtli) {
+            tabsCtli.classList.remove('active');
+        });
+        tabsCtLis[index].classList.add('active');
+    });
+});
