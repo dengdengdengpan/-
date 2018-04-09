17af8b44566be06baa3dc71cce962923337a2b83
dengdengdengpan <1306631506@qq.com>
2018-04-09 12:32:10 +0800

    1.完成.page-hot .hm-list的html和对应的css 2.完成.page-hot .hm-list的数据填充

--- js/index.js
+++ js/index.js
@@ -15,8 +15,7 @@ tabsLis.forEach(function(tabsli) {
     });
 });
 
-
-/*leancloud-storage*/
+/*leancloud-storage init*/
 var APP_ID = 'Mugo8DRz0JB2wQoHCfKzI40v-gzGzoHsz';
 var APP_KEY = 'vhMJxTzyQviG3EWuuW3QfPbN';
 
@@ -24,6 +23,7 @@ AV.init({
     appId: APP_ID,
     appKey: APP_KEY
 });
+
 /* 
     ***向leancloud上传并保存数据***
 let SongObject = AV.Object.extend('Song');
@@ -42,9 +42,10 @@ AV.Object.saveAll(songs).then(function (s) {
 });
 */
 
+/*向.page-rec .new-music .nm-list添加li元素节点*/
 let newList = document.querySelector('#content .page-rec .nm-list');
-let query = new AV.Query('Song');
-query.find().then(function(songs) {
+let queryNm = new AV.Query('Song');
+queryNm.find().then(function(songs) {
     for(let i = 0; i < songs.length; i++) {
         let song = songs[i].attributes;
         let li = document.createElement('li');
@@ -91,5 +92,69 @@ query.find().then(function(songs) {
         newList.appendChild(li);
     }
 }, function (error) {
-    alert('获取歌曲失败');
+    alert('获取新歌曲失败');
+});
+
+/*向.page-hot .hm-list添加li元素节点*/
+let hotMusicList = document.querySelector('#content .page-hot .hm-list');
+let queryHm = new AV.Query('Hot_song');
+queryHm.find().then(function(songs) {
+    for(let i = 0; i < songs.length; i++) {
+        let song = songs[i].attributes;
+        let li = document.createElement('li');
+        if (song.intro) {
+            li.innerHTML = `
+                <a href="#" class="hm-link">
+                    <div class="hm-rank">${song.rank}</div>
+                    <div class="hm-song">
+                        <div class="hm-sintro">
+                            <h3 class="hm-title">
+                                ${song.name}
+                                <span>${song.intro}</span>
+                            </h3>
+                            <p class="hm-author">
+                                <svg class="icon icon-sq" aria-hidden="true">
+                                    <use xlink:href="#icon-sq"></use>
+                                </svg>
+                                ${song.singer} - ${song.album}
+                            </p>
+                        </div>
+                        <div class="hm-play">
+                            <svg class="icon icon-play" aria-hidden="true">
+                                <use xlink:href="#icon-play"></use>
+                            </svg>
+                        </div>
+                    </div>
+                </a>
+            `; 
+        } else {
+            li.innerHTML = `
+                <a href="#" class="hm-link">
+                    <div class="hm-rank">${song.rank}</div>
+                    <div class="hm-song">
+                        <div class="hm-sintro">
+                            <h3 class="hm-title">
+                                ${song.name}
+                                <span>${song.intro}</span>
+                            </h3>
+                            <p class="hm-author">
+                                <svg class="icon icon-sq" aria-hidden="true">
+                                    <use xlink:href="#icon-sq"></use>
+                                </svg>
+                                ${song.singer} - ${song.album}
+                            </p>
+                        </div>
+                        <div class="hm-play">
+                            <svg class="icon icon-play" aria-hidden="true">
+                                <use xlink:href="#icon-play"></use>
+                            </svg>
+                        </div>
+                    </div>
+                </a>
+            `; 
+        }
+        hotMusicList.appendChild(li);
+    }
+}, function (error) {
+    alert('获取热歌榜失败');
 });
